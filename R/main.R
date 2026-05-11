dd28992949ee2161058c6006c3a38daed5193523
FlyBird <65411328+Larrycpan@users.noreply.github.com>
2026-05-11 16:10:16 +0800

    Update main.R

--- R/main.R
+++ R/main.R
@@ -2,36 +2,29 @@
 #'
 #' @param muts.input mutation input data frame as specified under input.format
 #' @param sig.input mutational signature reference with mutational classes as rows and signature exposures as columns: Substitution.Type, Trinucleotide, signature.1, ... signature.n
-#' @param input.format vcf: five column vcf-like data frame with the following columns: sample, chr (e.g. chr1), pos, ref, alt. classes: samples as columns and the 96 mutational classes as rows
+#' @param input.format sigs.input: pre-computed mutation count matrix in mut.to.sigs.input() output format (samples as rows, 96 mutational classes as columns). classes: samples as columns and the 96 mutational classes as rows
 #' @param sample.sigt.profs NULL = use all signatures provided in the reference. Optionally provide list with signatures to consider for each sample
 #' @param bootstrap TRUE/FALSE for whether bootstrapping is to be performed
 #' @param iterations number of bootstrapping iterations to perform (only if bootstrap == TRUE)
 #' @param strandbias TRUE/FALSE for whether transcriptional strand bias should be tested for (only for vcf-like input format)
-#' @param refcheck check that input mutational catalog (if vcf-format) is aligned to hg19
 #' @param cos_sim_threshold cosine similarity threshold below which signatures are removed from the final profile
 #' @param force_include vector with the names of signatures to always keep in the final profile of every sample
 #' @param dbg FALSE = silent; TRUE = verbose
 #' @importFrom dplyr left_join
-#' @importFrom deconstructSigs mut.to.sigs.input
-#' @import BSgenome.Hsapiens.UCSC.hg19
 #'
 #' @return mutational signature fitting results for all samples
 #' @export
 #'
 mm_fit_signatures = function(muts.input,
                              sig.input,
-                             input.format = "vcf",
+                             input.format = "sigs.input",
                              sample.sigt.profs=NULL,
                              bootstrap=FALSE,
                              iterations=1000,
                              strandbias=FALSE,
-                             refcheck=TRUE,
                              cos_sim_threshold=0.01,
                              force_include=c("SBS1", "SBS5"),
                              dbg=FALSE) {
-  "
-
-    "
 
   options(scipen = 999)
 
@@ -43,49 +36,44 @@ mm_fit_signatures = function(muts.input,
   consigts.defn <- sig.input
 
   # Input mutation data
-  if(input.format == "vcf") {
+  if(input.format == "sigs.input") {
 
-    if(sum(c("sample", "chr", "pos", "ref", "alt") %in% names(muts.input)) != 5){
-      stop("ERROR: Please provide the following data columns: sample, chr, pos, ref, alt")
+    # Validate that input looks like mut.to.sigs.input() output:
+    # rows = samples, cols = 96 trinucleotide mutation classes
+    if(!is.data.frame(muts.input) && !is.matrix(muts.input)){
+      stop("ERROR: muts.input must be a data frame or matrix in mut.to.sigs.input() output format
+           (samples as rows, 96 mutational classes as columns)")
     }
 
-    muts.input <- vcfCleaner(muts.input)
-
-    if(nrow(muts.input) == 0){
-      stop("ERROR: Inappropriate input data format")
+    if(ncol(muts.input) < 96){
+      stop("ERROR: muts.input must have at least 96 columns corresponding to trinucleotide mutation classes.
+           Please provide input in mut.to.sigs.input() output format.")
     }
 
-    if(refcheck){
-      if(!refCheck(muts.input, BSgenome.Hsapiens.UCSC.hg19)){
-        stop("ERROR: Wrong reference genome, please provide mutational data aligned to hg19/GRCh37")
-      }
-    }
-
-    # Input sample data
-    samples.muts <- mut.to.sigs.input(mut.ref = muts.input,
-                                      sample.id = "sample",
-                                      chr = "chr",
-                                      pos = "pos",
-                                      ref = "ref",
-                                      alt = "alt",
-                                      bsg = BSgenome.Hsapiens.UCSC.hg19)
-
-    samples.muts <- as.data.frame(t(samples.muts))
+    # Transpose: convert from (samples x classes) to (classes x samples)
+    # to match the internal format expected downstream
+    samples.muts <- as.data.frame(t(muts.input))
+    samples.muts <- samples.muts[1:96, , drop=FALSE]   # keep only the 96 mutation class rows
 
   } else if(input.format == "classes"){
+
+    # classes format: rows = 96 mutational classes, columns = samples
     samples.muts <- muts.input
-    samples.muts <- samples.muts[names(samples.muts) != "Total"]                       # remove totals column
-    samples.muts <- samples.muts[1:96,,drop=FALSE]                                     # remove additional rows (e.g. "Total)"
+    samples.muts <- samples.muts[names(samples.muts) != "Total"]        # remove totals column if present
+    samples.muts <- samples.muts[1:96, , drop=FALSE]                    # keep only the 96 mutation class rows
+
   } else{
-    stop("ERROR: Invalid input format, please provide a vcf-like format ('vcf') or 96 mutational classes ('classes')")
+    stop("ERROR: Invalid input format. Please provide:
+         'sigs.input' - mut.to.sigs.input() output format (samples as rows, 96 classes as columns), or
+         'classes'    - 96 mutational classes as rows, samples as columns")
   }
 
   # Process signature reference
   tcons.defn <- t(consigts.defn[,3:ncol(consigts.defn)])
   mutlist = paste(consigts.defn[,2],paste(substr(consigts.defn[,2],1,1),substr(consigts.defn[,1],3,3),substr(consigts.defn[,2],3,3),sep=""),sep=">")
-  consigts.defn <- sapply(consigts.defn[,3:ncol(consigts.defn)], as.numeric)  #n be cautious in the original the first two columns are included
-  rownames(consigts.defn)<-mutlist
-  ref_signatures <- colnames(consigts.defn) # names of signatures in reference
+  consigts.defn <- sapply(consigts.defn[,3:ncol(consigts.defn)], as.numeric)
+  rownames(consigts.defn) <- mutlist
+  ref_signatures <- colnames(consigts.defn)  # names of signatures in reference
 
   # Process sample mutational profiles
   samples <- colnames(samples.muts)
@@ -105,7 +93,7 @@ mm_fit_signatures = function(muts.input,
   }
 
   # Define output variable
-  output <-list()
+  output <- list()
 
   #####################################################
   ########       Fit mutational signatures     ########
@@ -152,16 +140,9 @@ mm_fit_signatures = function(muts.input,
   ########              Strand Bias            ########
   #####################################################
 
-  if(strandbias & input.format == "vcf"){
-
-    strand_bias_out <- getStrandBias(muts.input)
-
-    output$strand_bias_all_3nt <- strand_bias_out$all_3nt
-    output$strand_bias_mm1 <- strand_bias_out$mm1
-    output$strand_bias_SBS35 <- strand_bias_out$SBS35
-
-  } else if(strandbias & input.format != "vcf") {
-    warning("Transcriptional strand bias cannot be estimated from 96 classes input. \n Please provide vcf-like input format.")
+  if(strandbias){
+    warning("Transcriptional strand bias cannot be estimated from pre-computed mutation class input.
+             Please provide vcf-like input to a strand bias function separately.")
   }
 
   output$mutmatrix <- samples.muts
